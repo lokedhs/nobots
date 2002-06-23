74139dc6cffd88de68aadeea716b26fa3ba9b088
lokedhs <lokedhs>
2002-06-23 15:03:03 +0000

    Implemented the file selector for loading robots

---
+++ gtk/file_select.c
@@ -0,0 +1,38 @@
+#include <gnome.h>
+#include <stdio.h>
+
+#include "interface.h"
+#include "support.h"
+
+static GtkWidget *file_selector = NULL;
+
+static void robotfile_selected( void );
+
+void open_clicked( GtkButton *button, gpointer user_data )
+{
+  if( file_selector == NULL ) {
+    file_selector = gtk_file_selection_new( "Select robot descriptor file" );
+    gtk_signal_connect( GTK_OBJECT( GTK_FILE_SELECTION(file_selector)->ok_button ),
+			"clicked", GTK_SIGNAL_FUNC( robotfile_selected ), NULL );   gtk_signal_connect_object( GTK_OBJECT( GTK_FILE_SELECTION(file_selector)->ok_button ),
+													       "clicked", GTK_SIGNAL_FUNC( gtk_widget_hide ),
+													       (gpointer)file_selector);
+    
+    gtk_signal_connect_object( GTK_OBJECT( GTK_FILE_SELECTION(file_selector)->cancel_button ),
+			       "clicked", GTK_SIGNAL_FUNC( gtk_widget_hide ),
+			       (gpointer)file_selector );
+  }
+
+  gtk_widget_show( file_selector );
+}
+
+void remove_clicked( GtkButton *button, gpointer user_data )
+{
+  printf( "remove\n" );
+}
+
+static void robotfile_selected( void )
+{
+  gchar *filename = gtk_file_selection_get_filename( GTK_FILE_SELECTION(file_selector) );
+
+  printf( "file: %s\n", filename );
+}
